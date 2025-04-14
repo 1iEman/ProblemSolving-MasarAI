d28f791f579e362e460229bd90e30b39f1754bf9
Eman AL-Hajri <emaanhajri@gmail.com>
2025-04-14 08:54:10 +0400

    Update answer.py

--- answer.py
+++ answer.py
@@ -1,15 +1,16 @@
 
 # Example input: 4 4 3 2 4 --> Output: 3
-
-
 candles = list(map(int, input("Enter the candles: ").split()))
 
-
-longest = max(candles)
+highest_ = candles[0]
+for i in range(len(candles)):
+    if (candles[i] > highest_):
+        highest_ = candles[i]
 
 counter = 0
 for i in range(len(candles)):
-    if candles[i] == longest:
+    if candles[i] == highest_:
         counter += 1
 print(counter)
+
 
