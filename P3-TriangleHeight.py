805e8bb13848bcaad2bab7e387e516faf6d2983c
Eman AL-Hajri <emaanhajri@gmail.com>
2025-04-20 09:50:09 +0400

    Update P3-TriangleHeight.py

--- P3-TriangleHeight.py
+++ P3-TriangleHeight.py
@@ -6,63 +6,67 @@ Example:
 --------------------------------------
 ALgorithm : 
 1. Promote user to Input the height n.
+2. Loop from level = 1 to h:
 
-2. Initialize an empty list list1 to hold the left side and center value.
+    Initialize list1 as an empty list (this will hold the left side + center value).
 
-3. Initialize an empty list list2 to hold the right side (a mirror of the left side).
+    Initialize list2 as an empty list (this will hold the right side: mirror of the left).
 
-4. Loop from i = 0 to height - 2:
+    Loop from i = 0 to level - 2:
 
         Append 2^i to list1.
 
-5. Append 2 * last element of list1 to list1 (this is the center value).
+    Append 2^(level - 1) to list1 (this is the center value).
 
-6. Loop from j = 2 to height:
+    Loop from j = 2 to level:
 
-        Append the -jth element of list1 to list2.
+        Append list1[-j] to list2 (mirror the left).
 
-7. Merge list1 and list2 to create final list3.
+    Concatenate list1 and list2 into list3.
 
-8. Print list3.
+    Print the elements of list3.
+
 ---------------------------------------
 Pseudocode: 
-
 START
 INPUT h
-INITIALIZE list1 as empty list
-INITIALIZE list2 as empty list
 
-FOR i FROM 0 TO h - 2 DO
-    list1.APPEND(2 TO THE POWER OF i)
+FOR level FROM 1 TO h DO
+    INITIALIZE list1 as empty list
+    INITIALIZE list2 as empty list
+
+    FOR i FROM 0 TO level - 2 DO
+        APPEND 2^i TO list1
+    END FOR
+
+    APPEND 2^(level - 1) TO list1
+
+    FOR j FROM 2 TO level DO
+        APPEND list1[-j] TO list2
+    END FOR
+
+    SET list3 = list1 CONCATENATED WITH list2
+
+    PRINT elements of list3 space-separated
 END FOR
-
-list1.APPEND(last element of list1 TIMES 2)
-
-FOR j FROM 2 TO h DO
-    list2.APPEND(list1[-j])
-END FOR
-
-list3 = CONCATENATE list1 AND list2
-
-PRINT list3
-
 END
 '''
 
-h = int(input(" Enter Height: "))
+h = int(input("Enter Height: "))
 
-list1 = [] # Left Side + Centerd value
-list2 = [] # Right Side (Mirror of Left Side)
+for level in range(1, h + 1):
+    list1 = [] # Left Side + centerd value
+    list2 = [] # Right Side 
 
-for i in range(h - 1):
-    list1.append(2**i)
-    
-list1.append(list1[-1] * 2)  # Centerd Value
+    for i in range(level - 1):
+        list1.append(2 ** i)
 
+    list1.append(2 ** (level - 1))  # Center value
 
-for j in range(2, h + 1):
-    list2.append(list1[-j])
-    
-list3 = list1 + list2   # Merge them
-print(list3)
+    for j in range(2, level + 1):
+        list2.append(list1[-j])
+
+    list3 = list1 + list2 # Megre them
+    print(*list3)
+
 
